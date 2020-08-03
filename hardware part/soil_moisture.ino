873a5830da3a4768ecaf1198ef27cc66680a1be2
Harshit Kumar <50926910+harshuusaxena@users.noreply.github.com>
2020-08-03 14:29:22 +0530

    hardware code connect with cloud

--- hardware part/soil_moisture.ino
+++ hardware part/soil_moisture.ino
@@ -1,21 +1,34 @@
-#define BLYNK_MAX_SENDBYTES 256
-#define BLYNK_PRINT Serial
+
+//#define BLYNK_MAX_SENDBYTES 256
+//#define BLYNK_PRINT Serial
 
 #include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+//#include <FirebaseArduino.h>
+//#include <BlynkSimpleEsp8266.h>
 
-char auth[] = "3C-UaXJChYEmrBi1gOlUWt5U9DaGorde";
+
+
+////#define FIREBASE_HOST "sja-alumni.firebaseio.com"
+///#define FIREBASE_AUTH "qdM8yON8Kqy6zfaHLpYVoiutURlJ4bg2nIrI2ZuT"
+
+//char auth[] = "3C-UaXJChYEmrBi1gOlUWt5U9DaGorde";
+
+String apiKey = "EFOTEIN4HWLKMI5N"; 
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
 char ssid[] = "saxena family";
 char pass[] = "chitransh@545";
+const char* server = "api.thingspeak.com";
 
 const int sensor_pin = A0;  /* Connect Soil moisture analog sensor pin to A0 of NodeMCU */
 
+WiFiClient client;
+
 void setup() {
   Serial.begin(9600); /* Define baud rate for serial communication */
-  Blynk.begin(auth, ssid, pass);
+  //Blynk.begin(auth, ssid, pass);
+  //Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 void sendNotification(String message){
@@ -44,10 +57,49 @@ void loop() {
  Serial.print(moisture_percentage );
  Serial.println("%");
 
- if(moisture_percentage >=45 && moisture_percentage <=80)
- {
-  Blynk.notify("Be alert! Soil moisture level raise ..");
- }
-  Blynk.run();
-  delay(1000);
+// Firebase.pushString("Soil_Moisture", moisture_percentage);
+ delay(1000);
+
+  /*if (Firebase.failed()) 
+    {
+ 
+      Serial.print("pushing /logs failed:");
+      Serial.println(Firebase.error()); 
+      return;
+  }*/
+  if (isnan(moisture_percentage)) 
+                 {
+                     Serial.println("Failed to read from soil moisture sensor");
+                      return;
+                 }
+
+                         if (client.connect(server,80))   //   "184.106.153.149" or api.thingspeak.com
+                      {  
+                            
+                             String postStr = apiKey;
+                             postStr +="&field1=";
+                             postStr += String(moisture_percentage);
+             
+                             postStr += "\r\n\r\n";
+ 
+                             client.print("POST /update HTTP/1.1\n");
+                             client.print("Host: api.thingspeak.com\n");
+                             client.print("Connection: close\n");
+                             client.print("X-THINGSPEAKAPIKEY: "+apiKey+"\n");
+                             client.print("Content-Type: application/x-www-form-urlencoded\n");
+                             client.print("Content-Length: ");
+                             client.print(postStr.length());
+                             client.print("\n\n");
+                             client.print(postStr);
+ 
+                             Serial.print("Soil_Moisture: ");
+                             Serial.print(moisture_percentage);
+                             Serial.print(" % ");
+                        }
+          client.stop();
+ 
+          Serial.println("Waiting...");
+  
+  // thingspeak needs minimum 15 sec delay between updates, i've set it to 30 seconds
+  delay(10000);
 }
