bbc626b3c1b927af9347db36b0669512a075f26d
Harshit Kumar <importantemail545@gmail.com>
2020-08-03 15:04:53 +0530

    GPS error now fixed it

--- app/src/main/java/com/example/weather/activities/MapActivity.java
+++ app/src/main/java/com/example/weather/activities/MapActivity.java
@@ -42,7 +42,7 @@ public class MapActivity extends BaseActivity {
 
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("file:///android_asset/map.html?lat="+lat+"&lon="+lon+"&appid=" + mapViewModel.apiKey
+        webView.loadUrl("file:///android_asset/map.html?lat=" + mapViewModel.mapLat + "&lon=" + mapViewModel.apiKey
                 + "&zoom=" + mapViewModel.mapZoom);
         webView.addJavascriptInterface(new HybridInterface(), "NativeInterface");
         webView.setWebViewClient(new WebViewClient() {
